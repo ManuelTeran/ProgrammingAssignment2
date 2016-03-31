152c9f21119c432b648671677fe69b475c0fef3e
Manuel Terán <teran@itesm.mx>
2016-03-31 10:23:00 -0600

    Asignacion final

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##I took as a reference the makeVector function and customize to
+##take as input a matrix invertible, changing the
+##function mean by solve
 
+
+makeCaheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+##I took as a reference the cachemean function and customize to
+##take as input the result of the function makeCahceMatrix to
+##calculate the matrix inversa by the functionsolve if it were 
+##not calculated in cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
